67b594d9e16719df2f5b769bb8f723f95b7479e3
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-09-02 17:50:43 -0300

    exercício em C

---
+++ 2Periodo/Aula/Aula05/main.c
@@ -0,0 +1,32 @@
+//Número 1
+// #include <stdio.h>
+
+// float multiplicacao(float x, float y){
+//     return x*y;
+// }
+
+
+// int main(){
+
+//     float num1, num2;
+//     printf("Digite um numero: ");
+//     scanf("%f", &num1);
+//     printf("Digite um numero: ");
+//     scanf("%f", &num2);
+//     printf("A multiplicacao entre %.2f e %.2f : %.2f", num1, num2, multiplicacao(num1, num2));
+
+//     return 0;
+// }
+
+
+
+//Número 2
+#include <stdio.h>
+
+int votar(x){
+    
+}
+
+int main(){
+
+}
