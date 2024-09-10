cb663ea55adf21bf866d08df79f3ad293fbb16c7
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-09-10 16:38:38 -0300

    ?revisão de matéria

--- 2periodo/aula/aula03_matriz/main.c
+++ 2periodo/aula/aula03_matriz/main.c
@@ -1,44 +1,44 @@
  //Exercício 1
 
-//  #include <stdio.h>
+ #include <stdio.h>
 
-//  int main(){
+ int main(){
 
-//     int matrizA[3][2];
-//     int matrizB[2][3];
-//     int matrizResultado[3][3] = {0};
+    int matrizA[3][2];
+    int matrizB[2][3];
+    int matrizResultado[3][3] = {0};
 
-//     for(int i = 0; i < 3; i++){
-//         for(int j = 0; j < 2; j++){
-//             printf("Preencha a matriz A: ");
-//             scanf("%d", &matrizA[i][j]);
-//         }
-//     }
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 2; j++){
+            printf("Preencha a matriz A: ");
+            scanf("%d", &matrizA[i][j]);
+        }
+    }
 
-//     for(int i = 0; i < 2; i++){
-//         for(int j = 0; j < 3; j++){
-//             printf("Preencha a matriz B: ");
-//             scanf("%d", &matrizB[i][j]);
-//         }
-//     }
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 3; j++){
+            printf("Preencha a matriz B: ");
+            scanf("%d", &matrizB[i][j]);
+        }
+    }
 
-//     for(int i = 0; i < 3; i++){
-//         for(int j = 0; j < 3; j++){
-//             for(int k = 0; k < 2; k++){
-//                 matrizResultado[i][j] += matrizA[i][k] * matrizB[k][j];
-//             }
-//         }
-//     }
-//     printf("Resultado: \n");
-//     for(int i = 0; i < 3; i++){
-//         for(int j = 0; j < 3; j++){
-//             printf("[%d] ", matrizResultado[i][j]);
-//         }
-//         printf("\n");
-//     }
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            for(int k = 0; k < 2; k++){
+                matrizResultado[i][j] += matrizA[i][k] * matrizB[k][j];
+            }
+        }
+    }
+    printf("Resultado: \n");
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            printf("[%d] ", matrizResultado[i][j]);
+        }
+        printf("\n");
+    }
 
-//     return 0;
-//  }
+    return 0;
+ }
 
 
 
@@ -47,36 +47,36 @@
 
 
 // Exercício 2
-#include <stdio.h>
+// #include <stdio.h>
 
-int main (){
+// int main (){
 
-    int matriz[3][2];
-    int matrizEscalar[3][2];
-    int escalar = 0;
+//     int matriz[3][2];
+//     int matrizEscalar[3][2];
+//     int escalar = 0;
 
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 2; j++){
-            printf("Digite um numero da matriz: ");
-            scanf("%d", &matriz[i][j]);
-        }
-    }
-    printf("Digite o escalar: ");
-    scanf("%d", &escalar);
+//     for(int i = 0; i < 3; i++){
+//         for(int j = 0; j < 2; j++){
+//             printf("Digite um numero da matriz: ");
+//             scanf("%d", &matriz[i][j]);
+//         }
+//     }
+//     printf("Digite o escalar: ");
+//     scanf("%d", &escalar);
 
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 2; j++){
-            matrizEscalar[i][j] = escalar * matriz[i][j];
-        }
-    }
-    printf("Valor da matriz com escalar: \n");
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 2; j++){
-            printf("%d ", matrizEscalar[i][j]);
-        }
-        printf("\n");
-    }
+//     for(int i = 0; i < 3; i++){
+//         for(int j = 0; j < 2; j++){
+//             matrizEscalar[i][j] = escalar * matriz[i][j];
+//         }
+//     }
+//     printf("Valor da matriz com escalar: \n");
+//     for(int i = 0; i < 3; i++){
+//         for(int j = 0; j < 2; j++){
+//             printf("%d ", matrizEscalar[i][j]);
+//         }
+//         printf("\n");
+//     }
 
 
-    return 0;
-}
+//     return 0;
+// }
