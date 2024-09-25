2c2574c24826e9aa5bc60ddc6d1adb28071bfff1
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-09-24 22:13:40 -0300

    revisão aula ponteiro

--- 2periodo/aula/aula06_ponteiro/main.c
+++ 2periodo/aula/aula06_ponteiro/main.c
@@ -17,7 +17,7 @@
 
 
 
-//Número 1
+// Número 1
 
 // void maiorMenor(int vetor[], int *ptrmenor, int *ptrmaior);
 
@@ -73,22 +73,135 @@
 //     *media = soma / 5;
 // }
 
+// #include <string.h>
+
+// int main(){
+
+//     char frase[100];
+//     int tamanho = 0;
+
+//     printf("Digite uma frase: ");
+//     fgets(frase, sizeof(frase), stdin);
+//     tamanho = strlen(frase);
+//     for(int i = 0; i < strlen(frase); i++){
+//         if(frase[i] == ' ' || frase[i] == '\n'){
+//             tamanho--;
+//         }
+//     }
+//     printf("Sua frase é: %s\ne seu tamanho é: %d", frase, tamanho);
+
+//     return 0;
+// }
+
+
+
+// void trocar(int *pa, int *pb);
+
+// int main(){
+
+//     int a = 10, b = 20;
+//     trocar(&a, &b);
+//     printf("a = %d, b = %d", a, b);
+
+//     return 0;
+// }
+
+// void trocar(int *pa, int *pb){
+//     int tmp = *pb;
+//     *pb = *pa;
+//     *pa = tmp;
+// }
+
+
+// int main(){
+
+//     char frase[100];
+//     printf("Digite uma frase: ");
+//     fgets(frase, sizeof(frase), stdin);
+//     printf("A frase digitada foi: %s", frase);
+
+//     return 0;
+// }
+
+
+// void maiorMenor(int ve[], int *ma, int *me);
+
+// int main(){
+
+//     int array[4], maior = 0, menor = 0;
+//     for(int i = 0; i < 4; i++){
+//         printf("Digite um valor: ");
+//         scanf("%d", &array[i]);
+//     }
+//     maiorMenor(array, &maior, &menor);
+//     printf("O maior e: %d\nO menor e: %d", maior, menor);
+
+//     return 0;
+// }
+
+// void maiorMenor(int ve[], int *ma, int *me){
+//     for(int i = 0; i < 4; i++){
+//         if(*(ve + i) > *ma){
+//             *ma = *(ve + i);
+//         }
+//         if(*(ve + i) < *me || i == 0){
+//             *me = *(ve + i);
+//         }
+//     }
+// }
+
+// int mediaArray(float array[], float *so, float *me);
+
+// int main(){
+
+//     float array[5], soma = 0, media = 0;
+//     float *ptrsoma = &soma, *ptrmedia = &media;
+//     for(int i = 0; i < 5; i++){
+//         printf("Digite um valor: ");
+//         scanf("%f", &array[i]);
+//     }
+
+//     mediaArray(array, ptrsoma, ptrmedia);
+//     printf("A media e: %.2f", media);
+
+//     return 0;
+// }
+
+// int mediaArray(float array[], float *so, float *me){
+//     for(int i = 0; i < 5; i++){
+//         *so += *(array + i);
+//     }
+//     *me = *so / 5;
+//     return *me;
+// }
+
 #include <string.h>
+
+void contar_caracteres(char *str, int *numCaracteres);
 
 int main(){
 
-    char frase[100];
-    int tamanho = 0;
+    char string[50];
+    int qtd = 0;
+    printf("Digite uma string: ");
+    fgets(string, sizeof(string), stdin);
 
-    printf("Digite uma frase: ");
-    fgets(frase, sizeof(frase), stdin);
-    tamanho = strlen(frase);
-    for(int i = 0; i < strlen(frase); i++){
-        if(frase[i] == ' ' || frase[i] == '\n'){
-            tamanho--;
-        }
+    if(string[strlen(string) - 1] == '\n'){
+        string[strlen(string) - 1] = '\0';
     }
-    printf("Sua frase é: %s\ne seu tamanho é: %d", frase, tamanho);
 
+    contar_caracteres(string, &qtd);
+
+    printf("A string possui %d caracteres", qtd);
     return 0;
 }
+
+void contar_caracteres(char *str, int *numCaracteres){
+    *numCaracteres = 0;
+    while(str[*numCaracteres] != '\0'){
+        (*numCaracteres)++;
+    }
+}
+
+
+
