5d8d33de5b468cf77f8abbf31a1307217e9627e0
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-10-08 23:18:17 -0300

    revisão ponteiro

---
+++ exercicios/testes_ponteiro/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+//!Inverter valores 
+// void inverter(int *pa, int *pb);
+
+// int main(){
+
+//     int a = 10, b = 20;
+//     int *pa = &a, *pb = &b;
+//     inverter(pa, pb);
+//     printf("a = %d, b = %d", a, b);
+//     return 0;
+// }
+
+// void inverter(int *pa, int *pb){
+//     int temp = *pa;
+//     *pa = *pb;
+//     *pb = temp;
+// }
+
+
+// void media(int vetor[], int *media);
+
+// int main(){
+//     int vetor[5], med;
+//     for(int i = 0; i < 5; i++){
+//         printf("Digite a nota %d", i+1);
+//         scanf("%d", &vetor[i]);
+//     }
+//     media(vetor, &med);
+//     printf("Media: %d: ", med);
+// }
+
+// void media(int vetor[], int *media){
+//     int soma = 0;
+//     for(int i = 0; i < 5; i++){
+//         soma += *(vetor + i);
+//     }
+//     *media = soma / 5;
+// }
+
+
+
