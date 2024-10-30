b31e351b949ee8da66f6037436db6b70b8a3831c
Natan Pereira Braslavsky de Oliveira <natanbraslavsky1@gmail.com>
2024-10-30 13:34:59 +0000

    aula 09 struct

---
+++ 2periodo/aula/aula09_structs/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// struct aluno{
+//     char nome[50];
+//     float notas[3];
+// };
+// typedef struct aluno aluno;
+
+// float maiorNota(aluno *aluno);
+
+// int main(){
+
+//     aluno aluno;
+//     float maior = 0;
+//     printf("Digite seu nome: ");
+//     fgets(aluno.nome, sizeof(aluno.nome), stdin);
+//     for(int i = 0; i < 3; i++){
+//         printf("Nota %d: ", i+1);
+//         scanf("%f", &aluno.notas[i]);
+//     }
+//     maior = maiorNota(&aluno);
+//     printf("Maior nota: %.2f", maior);
+
+//     return 0;
+// }
+
+// float maiorNota(aluno *aluno){
+//     float maior = 0;
+//     for(int i = 0; i < 3; i++){
+//         if(aluno->notas[i] > maior){
+//             maior = aluno->notas[i];
+//         }
+//     }
+//     return maior;
+// }
+
+
+
+
