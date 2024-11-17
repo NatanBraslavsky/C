82f2f0252601001991e0947313aef85dc21cc6d4
NatanBraslavsky <natanbraslavsky1@gmail.com>
2024-11-17 17:29:46 -0300

    numero 2 tde 3

--- 2periodo/tdes/tde03/main.c
+++ 2periodo/tdes/tde03/main.c
@@ -1,34 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct livro {
-    char titulo[30], autor[30];
-    int numPaginas;
+
+//!Numero 1
+// struct livro {
+//     char titulo[30], autor[30];
+//     int numPaginas;
+// };
+
+// typedef struct livro livro;
+
+// void informacoes(livro livro);
+
+// int main(){
+//     livro livro;
+
+//     printf("Digite o titulo do livro: ");
+//     fgets(livro.titulo, sizeof(livro.titulo), stdin);
+
+//     printf("Digite o autor do livro: ");
+//     fgets(livro.autor, sizeof(livro.autor), stdin);
+
+//     printf("Numero de paginas: ");
+//     scanf("%d", &livro.numPaginas);
+
+//     informacoes(livro);
+
+//     return 0;
+// }
+
+// void informacoes(livro livro){
+//     printf("\nTitulo: %s", livro.titulo);
+//     printf("Autor: %s", livro.autor);
+//     printf("Numero de paginas: %d", livro.numPaginas);
+// }
+
+
+
+//!Numero 2
+
+struct cadastroAluno{
+    char nomeAluno[30];
+    int numMatricula;
+    float notas[3];
 };
 
-typedef struct livro livro;
+typedef struct cadastroAluno aluno;
 
-void informacoes(livro livro);
+void mediaNotas(aluno al);
 
 int main(){
-    livro livro;
+    aluno al;
 
-    printf("Digite o titulo do livro: ");
-    fgets(livro.titulo, sizeof(livro.titulo), stdin);
+    printf("Digite o nome do aluno: ");
+    fgets(al.nomeAluno, sizeof(al.nomeAluno), stdin);
 
-    printf("Digite o autor do livro: ");
-    fgets(livro.autor, sizeof(livro.autor), stdin);
+    printf("Digite a matricula do aluno: ");
+    scanf("%d", &al.numMatricula);
 
-    printf("Numero de paginas: ");
-    scanf("%d", &livro.numPaginas);
+    for(int i = 0; i < 3; i++){
+        printf("Nota %d: ", i+1);
+        scanf("%f", &al.notas[i]);
+    }
 
-    informacoes(livro);
-
+    mediaNotas(al);
+    
     return 0;
 }
 
-void informacoes(livro livro){
-    printf("\nTitulo: %s", livro.titulo);
-    printf("Autor: %s", livro.autor);
-    printf("Numero de paginas: %d", livro.numPaginas);
+void mediaNotas(aluno al){
+    float soma;
+    float media;
+    for(int i = 0; i < 3; i++){
+        soma+= al.notas[i];
+    }
+    media = soma / 3;
+    printf("Media: %.2f", media);
 }
+
+
