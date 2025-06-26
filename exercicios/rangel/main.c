acabcc3498471fee46e8a5db1a5cdb65302a2072
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-06-26 16:32:59 -0300

    estudos java

---
+++ exercicios/rangel/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+struct ContaBancaria{
+    int numeroConta;
+    char nomeTitular[50];
+    float saldo;
+}
+typedef struct ContaBancaria contabancaria;
+
+
+int main(void){
+    contabancaria conta;
+    
+
+    return 0;
+}
